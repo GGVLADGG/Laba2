a98c0e6049abd9c8d7a45eb016f110d8a445cb8e
Vlad <V.Dotsenko.FIT.121.20@knute.edu.ua>
2021-09-27 00:34:22 +0300

    first commit

--- Lab2Work4/Lab2Work4/Program.cs
+++ Lab2Work4/Lab2Work4/Program.cs
@@ -5,45 +5,73 @@ namespace Lab2Work4
 {
     class Point
     {
-        private static int x;
-        private static int y;
-        private static string NamePoint;
-        public static int g;
-        public static int X
+        public int X { get; }
+        public int Y { get; } 
+        public string Name { get; }
+        public Point(int x, int y, string name)
         {
-            get
-            {
-                return x;
-            }
-        }
-        public static int Y
-        {
-            get
-            {
-                return y;
-            }
-        }
-        public static string name
-        {
-            get
-            {
-                return NamePoint;
-            }
-        }
-        public Point(int x, int y, string NamePoint)
-        {
-            g = x;
+            X = x;
+            Y = y;
+            Name = name;
         }
     }
     class Figure
     {
-        public static void Input()
+        public static int input;
+        public Point[] points { get; }
+        public Figure(Point point1, Point point2, Point point3)
         {
-
+            points = new Point[] { point1, point2, point3 };
         }
-        public Figure()
+        public Figure(Point point1, Point point2, Point point3, Point point4)
         {
-            
+            points = new Point[] { point1, point2, point3, point4 };
+        }
+        public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+        {
+            points = new Point[] { point1, point2, point3, point4, point5 };
+        }
+        public double LengthSize(Point point1, Point point2)
+        {
+            return Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+        }
+        public void PerimeterCalculator()
+        {
+            double Perimeter = 0;
+            for (int i = 0; i < input ; i++)
+            {
+                if (i == points.Length - 1)
+                {
+                    Perimeter += LengthSize(points[i], points[0]);
+                }
+                else
+                {
+                    Perimeter += LengthSize(points[i], points[i + 1]);
+                }
+            }
+            Console.WriteLine("Периметр фігури: " + Perimeter);
+        }
+        public void FigureInitialization()
+        {
+            Console.WriteLine("Введіть кількість кутів( від 3 до 5 ): ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input < 3 || input > 5)
+            {
+                Console.WriteLine("Помилка при введенні");
+                Environment.Exit(-1);
+            }
+            if (input == 3)
+            {
+                Console.WriteLine("Фігура: трикутник");
+            }
+            else if (input == 4)
+            {
+                Console.WriteLine("Фігура: чотирикутник");
+            }
+            else if (input == 5)
+            {
+                Console.WriteLine("Фігура: п'ятикутник");
+            }
         }
     }
     class Program
@@ -54,12 +82,10 @@ namespace Lab2Work4
             var enc1251 = Encoding.GetEncoding(1251);
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = enc1251;
-            Point A = new Point(0, 3, "A");
-            Point B = new Point(0, 0, "B");
-            Point C = new Point(3, 0, "C");
-            Point D = new Point(3, 3, "D");
-            Point E = new Point(4, 4, "E");
-            Figure NewFigure = new Figure();
+            Figure figure = new Figure(new Point(0, 2, "A"), new Point(0, 0, "B"), new Point(2, 0, "C"), new Point(2, 2, "D"), new Point(1, 1, "E"));
+            figure.FigureInitialization();
+            figure.PerimeterCalculator();
+            Console.ReadLine();
         }
     }
 }
